d135d10e46925a8d59a76d1163ca3b1dc37fb019
4peat <xxxpaction@gmail.com>
2024-07-30 18:42:05 +0300

    Added Book class fields

--- src/main/java/library/models/Book.java
+++ src/main/java/library/models/Book.java
@@ -1,7 +1,7 @@
 package library.models;
 
 public class Book {
-    private String id;
+    private String id = "eqw";
     private String title;
     private String author;
     private boolean isBorrowed;
